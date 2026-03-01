7c29c97e5a0107c4bc103fbffb83049fda117bba
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-03-02 11:29:42 +1300

    Declared two new variables. Those being homeAddress and userAge.

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ score = 10;   //OK - reassignment allowed for let
 
 const MAX = 100;
 // MAX = 200;   //TypeError: Assignment to constant variable
+
+let homeAddress = "58 Somerset Terrace";   // Declared a new variable named homeAddress and assigned it my home address.
+let userAge = "30";   // Declared a new variable named userAge and assigned it the value of 30.   
+console.log(homeAddress)
